98e91bc88f39dc773c584d073588db8897b003e0
lixingyao <lixingyao001@lianjia.com>
2019-11-14 20:36:30 +0800

    修改

--- all_import.sh
+++ all_import.sh
@@ -17,11 +17,9 @@ for file in ` ls $1 `
             long_d_suffix=${fileName:0-4:4}
             if test "$d_suffix" = "$m_suffix" || test "$d_suffix" = "$h_suffix"
             then
-                echo "hhhhhhh"$fileName
                 findWord $fileName
             elif test "$long_d_suffix" = "$pch_suffix"
             then
-                echo "pchpchpchpch"$fileName
                 findWord $fileName
             fi
         fi
@@ -29,41 +27,41 @@ done
 }
 
 function findWord(){
-    num=1
     cat $1 | while read line
     do
         result=$(echo $line | grep "#import <")
         if [[ "$result" != "" ]]
         then
-            echo $line>> findWord.txt
+            result=$(echo $line | grep "/")
+            if [[ "$result" != "" ]]
+            then
+                real_name=${line#*<}
+                real_name=${real_name%%/*}
+            else
+                real_name=${line#*<}
+                real_name=${real_name%%>*}
+            fi
+            echo $real_name>> findWord.txt
+        else
+        result=$(echo $line | grep "#import \"")
+            if [[ "$result" != "" ]]
+            then
+                #截取字符串
+                real_name=${line#*\"}
+                real_name=${real_name%%\"}
+
+                if [ `grep -c $real_name "all_file.txt"` -le '0' ];
+                then
+                    echo $real_name>> findWord.txt
+                fi
+            fi
         fi
-    num=$((num+1))
     done
     checkReplace "findWord.txt"
 }
 
 function checkReplace() {
     sort $1 |uniq > checkReplace.txt
-    subSplitStr "checkReplace.txt"
-}
-
-
-#截取字符串
-function subSplitStr() {
-    cat $1 | while read line;
-    do
-       result=$(echo $line | grep "/")
-       if [[ "$result" != "" ]]
-        then
-          real_name=${line#*<}
-          real_name1=${real_name%%/*}
-        else
-            real_name=${line#*<}
-            real_name1=${real_name%%>*}
-        fi
-        echo $real_name1>> subSplitStr.txt
-    done
-     sort "subSplitStr.txt" |uniq > end_result.txt
 }
 
 
@@ -76,10 +74,32 @@ function find_file() {
         fi
     done
 }
-left_file=$1
-right_file=$2
+
+function find_all_file () {
+
+for file in ` ls $1 `
+    do
+        if [ -d $1"/"$file ]
+        then
+            find_all_file $1"/"$file
+        else
+            fileName=$1"/"$file
+            fileName=${fileName##*/}
+
+            d_suffix=${fileName:0-2:2}
+            long_d_suffix=${fileName:0-4:4}
+            if test "$d_suffix" = "$m_suffix" || test "$d_suffix" = "$h_suffix" || test "$long_d_suffix" = "$pch_suffix"
+            then
+                echo $fileName>> all_file.txt
+            fi
+            
+        fi
+done
+}
 
 
 #测试目录 test
+find_all_file $1
 read_dir $1
-find_file "end_result.txt" $right_file
+find_file "checkReplace.txt" $2
+
