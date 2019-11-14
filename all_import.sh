36799afa2a43329aa6ff047f386010ef300cfe0c
Yi Li <liyi02@lianjia.com>
2019-11-15 00:13:07 +0800

    最终版

--- all_import.sh
+++ all_import.sh
@@ -4,6 +4,11 @@
 m_suffix=".m"
 h_suffix=".h"
 pch_suffix="pch"
+repo_name=""
+first_param=$1
+second_param=$2
+third_param=$3
+library_name=""
 
 function read_dir(){
 for file in ` ls $1 `
@@ -24,6 +29,25 @@ for file in ` ls $1 `
             fi
         fi
 done
+}
+
+function findRepoName(){
+    for file in ` ls $1 `
+    do
+        if [ -d $1"/"$file ]
+        then
+            findRepoName $1"/"$file
+        else
+            fileName=$1"/"$file
+            repo_real_name=${fileName##*/}
+            if test "$repo_real_name" = "$repo_name"
+            then
+            deal_name=${fileName##*Public/}
+            deal_name=${deal_name%%/*}
+            library_name=$deal_name
+            fi
+        fi
+    done
 }
 
 function findWord(){
@@ -49,14 +73,18 @@ function findWord(){
                 #截取字符串
                 real_name=${line#*\"}
                 real_name=${real_name%%\"}
-
+                repo_name=$real_name
                 if [ `grep -c $real_name "all_file.txt"` -le '0' ];
                 then
-                    library_name="library_name"
-                    echo $library_name>> findWord.txt
-                    new_string="<$library_name\/$real_name>"
-                    origin_string="\"$real_name\""
-                    sed -i "" "s/$origin_string/$new_string/g" "$1"
+                    library_name=""
+                    findRepoName $third_param
+                    if [[ "$library_name" != "" ]]
+                    then
+                        echo $library_name>> findWord.txt
+                        new_string="<$library_name\/$real_name>"
+                        origin_string="\"$real_name\""
+                        sed -i "" "s/$origin_string/$new_string/g" "$1"
+                    fi
                 fi
             fi
         fi
