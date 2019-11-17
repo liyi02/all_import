5b0665a227d554c49268504f7b27b875d8ee6757
Yi Li <liyi02@lianjia.com>
2019-11-17 23:53:04 +0800

    判断是不是这个库

--- git_show_log.sh
+++ git_show_log.sh
@@ -2,7 +2,57 @@
 cd `dirname $0`
 dir=`pwd`
 h_suffix=".h"
+library_name=""
 
+#寻找是不是库名
+function is_repo_name(){
+    result=$(echo `ls "../Lianjia_iOS_Shell_Project/Pods/"` | grep $1)
+    if [[ "$result" != "" ]]
+    then
+        echo isARepo
+        return 1
+    fi
+    return 0
+}
+
+#寻找此此文件是否在当前仓库
+function is_current_repo(){
+    result=`ls -R |grep -v ^d | awk '{print $1}' | grep $1`
+    if [[ "$result" != "" ]]
+    then
+        return 1
+    fi
+    return 0
+}
+
+#寻找此仓库是否被正确依赖
+function is_repo_dependent(){
+    result=`echo "$(dirname "$PWD").podspec" | grep $1`
+    if [[ "$result" != "" ]]
+    then
+        return 1
+    fi
+    return 0
+}
+
+function finish_execute(){
+ rm -f all_diff.txt
+ exit $1
+}
+
+function find_library_name(){
+    library_name=""
+    ls -R |grep -v ^d | awk '{print $1}' | while read line
+    do
+        repo_real_name=${line##*/}
+        if test "$repo_real_name" = "$1"
+        then
+            deal_name=${fileName##*Public/}
+            deal_name=${deal_name%%/*}
+            library_name=$deal_name
+        fi
+    done
+}
 
 git show> all_diff.txt
 cat all_diff.txt | while read line
@@ -29,16 +79,56 @@ do
                 real_name_after=${real_name_after_pex%%>*}
                 #对库名real_name_before处理，搜索不到库名
                 #对real_name_after进行处理
-                echo $real_name_before>> findWord.txt
-                echo $real_name_after>> findWord.txt
-                #调用易哥的方法
+                
+                is_repo_name $real_name_before
+                if test $? = "1"
+                then #如果/前边的是仓库名
+                    is_repo_dependent $real_name_before
+                    if test $? = 1 #如果是仓库名且本仓库正确依赖了这个仓库
+                    then
+                        finish_execute 0
+                    else
+                        echo ${real_name_before}没有被这个仓库依赖，请正确依赖
+                        finish_execute 1;
+                    fi
+                else #如果/前边的不是仓库名，匹配/后面的
+                    find_library_name $real_name_after
+                    if [[ "$library_name" != "" ]]
+                    then
+                        is_repo_dependent $library_name
+                        if test $? = 1#本仓库正确依赖了这个.h文件所在仓库
+                        then
+                            finish_execute 0;
+                        else #本仓库没有正确依赖了这个.h文件所在仓库
+                            echo ${real_name_before}没有被这个仓库依赖，请正确依赖
+                            finish_execute 1;
+                        fi
+                    fi
+                fi
+
             else
                 #场景3#import<B.h>
                 real_name=${line#*<}
                 real_name=${real_name%%>*}
-                #对real_name进行处理
-                echo  $real_name>> findWord.txt
-                #调用易哥的方法
+                #判断当前库中是否包含这个头文件
+                is_current_repo $real_name
+                if [[ "$?" != "" ]]
+                then
+                    finish_execute 0;
+                fi
+                #当前库不包含这个头文件，找到这个文件所在
+                find_library_name $real_name
+                if [[ "$library_name" != "" ]]
+                then
+                    is_repo_dependent $library_name
+                    if test $? = 1#本仓库正确依赖了这个.h文件所在仓库
+                    then
+                        finish_execute 0;
+                    else #本仓库没有正确依赖了这个.h文件所在仓库
+                        echo ${real_name_before}没有被这个仓库依赖，请正确依赖
+                        finish_execute 1;
+                    fi
+                fi
             fi
         else
         result=$(echo $line | grep "#import \"")
@@ -54,21 +144,60 @@ do
                 #后面的.h
                 real_name_after_pex=${real_name##*/}
                 real_name_after=${real_name_after_pex: -1}
-                #对库名real_name_before处理，搜索不到库名
-                #对real_name_after进行处理
-                echo  $real_name_before>> findWord.txt
-                echo  $real_name_after>> findWord.txt
+                
+                is_repo_name $real_name_before
+                if test $? = 1
+                then #如果/前边的是仓库名
+                    is_repo_dependent $real_name_before
+                    if test $? = 1 #如果是仓库名且本仓库正确依赖了这个仓库
+                    then
+                        finish_execute 0;
+                    else
+                        echo ${real_name_before}没有被这个仓库依赖，请正确依赖
+                        finish_execute 1;
+                    fi
+                else #如果/前边的不是仓库名，匹配/后面的
+                    find_library_name $real_name_after
+                    if [[ "$library_name" != "" ]]
+                    then
+                        is_repo_dependent $library_name
+                        if test $? = 1#本仓库正确依赖了这个.h文件所在仓库
+                        then
+                            finish_execute 0;
+                        else #本仓库没有正确依赖了这个.h文件所在仓库
+                            echo ${real_name_before}没有被这个仓库依赖，请正确依赖
+                            finish_execute 1;
+                        fi
+                    fi
+                fi
+                
                 #调用易哥的方法
                 else
                     # 场景4#importB.h
                     real_name=${line#*\"}
                     real_name=${real_name%%\"}
-                    echo `ls -R "./"`>result.txt
                     result=$(echo `ls -R "./"`|grep $real_name)
                     if [[ "$result" == "" ]]
                     then
                         #判断当前库中是否包含这个头文件
-                        echo $real_name>> findWord.txt
+                        is_current_repo $real_name
+                        if [[ "$?" != "" ]]
+                        then
+                            finish_execute 0;
+                        fi
+                        #当前库不包含这个头文件，找到这个文件所在
+                        find_library_name $real_name
+                        if [[ "$library_name" != "" ]]
+                        then
+                            is_repo_dependent $library_name
+                            if test $? = 1#本仓库正确依赖了这个.h文件所在仓库
+                            then
+                                finish_execute 0;
+                            else #本仓库没有正确依赖了这个.h文件所在仓库
+                                echo ${real_name_before}没有被这个仓库依赖，请正确依赖
+                                finish_execute 1;
+                            fi
+                        fi
                         #不包含就对real_name进行处理
                         #调用易哥的方法
      
@@ -78,6 +207,7 @@ do
         fi
     fi
 done
+
 
 
 
