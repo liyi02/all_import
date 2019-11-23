acc775580ed3736332ceb3c1a0b04eff5ba2df91
Yi Li <liyi02@lianjia.com>
2019-11-23 17:49:25 +0800

    就是这次提交

--- git_show_log.sh
+++ git_show_log.sh
@@ -1,100 +1,66 @@
-
 cd `dirname $0`
 dir=`pwd`
 change_name="false"
 
+
+
 #寻找此此文件是否在当前仓库
-function is_current_repo(){
-podspec_name=${dir##*/}
-    result=`ls -R ${dir}/${podspec_name} |grep -v ^d | awk '{print $1}' | grep $1`
-    if [[ "$result" != "" ]]
-    then
-        return 1
-    fi
-    return 0
-}
-
-#寻找此仓库是否被正确依赖
-function is_repo_dependent(){
+function is_current_repo() {
     podspec_name=${dir##*/}
-    result=`cat "${dir}/${podspec_name}.podspec" | grep $1`
+    result=`ls -R ${dir}/${podspec_name} | grep -v ^d | awk '{print $1}' | grep $1`
     if [[ "$result" != "" ]]
     then
-        return 1
+        return 0
     fi
-    return 0
+    return 1
 }
 
-git show> all_diff.txt
-sort all_diff.txt |uniq > new_all_diff.txt
+#寻找此仓库是否正确依赖
+function is_correct_dependent() {
+    podspec_name=${dir##*/}
+    result=`cat ${dir}/${podspec_name}.podspec | grep $1`
+    if [[ "$result" != "" ]]
+    then
+        return 0
+    fi
+    return 1
+}
+echo "`git show | sort | uniq | grep '^+*#import'`">result.txt
 
 while read line
 do
-    result=$(echo $line | grep "^+")
-    if [[ "$result" != "" ]]
-    then
-        result=$(echo $line | grep "#import <")
-        if [[ "$result" != "" ]]
+    file_name=`echo ${line} |awk -F '[/]' '{print $NF}' |awk -F '["]' '{print $(NF-1)}' |awk -F '[>]' '{print $(NF-1)}'`
+    is_current_repo $file_name
+    if [[ "$?" == "0" ]]
         then
-            result=$(echo $line | grep "/")
-            if [[ "$result" != "" ]]
-            then
-                real_name=${line#*<}
-                real_name_before=${real_name%%/*}
-                is_repo_dependent $real_name_before
-                if test $? = 0
-                then
-                change_name="true"
-                echo ${real_name_before}"没有被这个仓库依赖，请正确依赖"
-            fi
-            else
-                real_name=${line#*<}
-                real_name=${real_name%%>*}
-                change_name="true"
-                echo ${real_name}"其他仓库的头文件不可以使用<>不带库名的方式引入，请修改"
-            fi
+           result=$(echo ${line} | grep $file_name | grep '>')
+           if [[ "$result" != "" ]]
+           then
+           echo ${file_name}"这个头文件是本库中的，不需要尖括号方式引用，请修改"
+           change_name="true"
+           fi
+    else
+        result=$(echo ${line} | grep $file_name | grep '<' | grep '>' | grep '/')
+        if [[ "$result" == "" ]]
+        then
+            echo ${file_name}"这个头文件是其他库中的，需要用尖括号库名斜杠类名方式引用，请修改"
+            change_name="true"
         else
-        result=$(echo $line | grep "#import \"")
-            if [[ "$result" != "" ]]
+            repo_name=`echo ${line} |awk -F '[/]' '{print $1}' | awk -F '[<]' '{print $NF}'`
+            is_correct_dependent $repo_name
+            if [[ "$?" == "1" ]]
             then
-                result=$(echo $line | grep "/")
-                if [[ "$result" != "" ]]
-                then
-                    real_name=${line#* \"}
-                    real_name_before=${real_name%%/*}
-                    is_repo_dependent $real_name_before
-                    if test $? = 0
-                    then
-                    change_name="true"
-                    echo ${real_name_before}"没有被这个仓库依赖，请正确依赖"
-                    fi
-                else
-                    real_name=${line#*\"}
-                    real_name=${real_name%%\"}
-                    is_current_repo $real_name
-                    if [[ "$?" == "0" ]]
-                    then
-                    change_name="true"
-                    echo ${real_name}"不是本仓库的头文件，请使用<库名/类名>的方式引用"
-                    fi
-                 fi
+                echo $repo_name"没有被本仓库依赖，请正确依赖"
             fi
+            library_name=${line%/*}
         fi
     fi
-done< new_all_diff.txt
+done< result.txt
 
-rm -f all_diff.txt
-rm -f new_all_diff.txt
 
 if [ -z $change_name ]
     then
-    exit 0
+    exit 1
 else
     exit 1
 fi
-
-
-
-
-
-
